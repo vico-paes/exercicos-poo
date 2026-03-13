8ea4b45691f5c5eb2113350b4bfe3193f2d30216
Vinicius Paes <vinicius.paes@thunders.com.br>
2026-03-13 16:32:19 -0300

    Primeiro exercico com coisas novas

---
+++ PrimieroEX/PrimieroEX/Produto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimieroEX
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Qunt;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Qunt;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Qunt = Qunt + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Qunt = Qunt - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return $"Produto: {Nome}, Preço: {Preco.ToString("F2")}, Quantidade: {Qunt}, Valor total em estoque: {ValorTotalEmEstoque().ToString("F2")}";
+        }
+    }
+}
---
+++ PrimieroEX/PrimieroEX/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PrimieroEX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+
+            Console.WriteLine("Coloque o o produto desejado, o preço dele e a quantidade em estoque");
+            string[] entrada = Console.ReadLine().Split(' ');
+            produto.Nome = entrada[0];
+            produto.Preco = double.Parse(entrada[1]);
+            produto.Qunt = int.Parse(entrada[2]);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + produto);
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atulizados: " + produto);
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o número de produtos a ser removido do estoque: ");
+            produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atulizados: " + produto);
+        }
+    }
+}
