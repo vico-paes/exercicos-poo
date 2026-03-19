ade8187f32dac2bd2de126b2c48fad731a3e585b
Vinicius Paes <vinicius.paes@thunders.com.br>
2026-03-19 16:41:53 -0300

    Encapsulameto, Constutores, This e Sobrecarga

---
+++ ExFinal5/ExFinal5/ContaBancaria.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExFinal5
+{
+    internal class ContaBancaria
+    {
+        // o get vc nunca podera mudar, o set vc pode mudar, o private vc so pode mudar dentro da propria classe //
+        public int NumeroConta { get; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        // public double Deposito { get; private set; } , jeito que o Andre ensinou//
+
+        // this pega o de fora //
+        public ContaBancaria(int numeroConta, string titular, double saldo) //int deposito =0//)
+        {
+            this.NumeroConta = numeroConta;
+            this.Titular = titular;
+            this.Saldo = saldo;
+            // this.Deposito = deposito;//
+        }
+
+        /*o nelio faz 2 construtrores, 1 com o saldo outro sem o saldo, e no que tem o saldo
+        ele usa um Deposito(saldo), pq caso mude algo no sacar ou depositar ele muda aqui tbm*/
+        public void depositar(double valor)
+        {
+            Saldo += valor;
+        }
+        public void sacar(double valor)
+        {
+            Saldo -= valor + 5.00;
+        }
+        public override string ToString()
+        {
+            return $"{NumeroConta}, Titular: {Titular}, Saldo: ${Saldo.ToString("F2")}";
+        }
+
+    }
+}
---
+++ ExFinal5/ExFinal5/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ExFinal5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* nao sei como fazer sem o deposito estar aqui, no video da resolução ele cria 2 construtrores e depopis
+          incia dentro do if else, (Andre ensinou um jeito)*/
+            double deposito = 0;
+
+            Console.Write("Digite o numero da conta: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome da conta: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            string resposta = Console.ReadLine();
+
+            while (resposta != "s" && resposta != "n")
+            {
+                Console.WriteLine("Resposta inválida. Por favor, digite 's' para sim ou 'n' para não.");
+                resposta = Console.ReadLine();
+            }
+
+            if (resposta == "s")
+            {
+                Console.Write("Digite o valor do depósito inicial: ");
+                deposito = double.Parse(Console.ReadLine());
+            }
+
+            //sempre que tiver privete vc le primerio depois inicia//
+            ContaBancaria conta = new ContaBancaria(num, nome, deposito);
+
+            Console.WriteLine("Dados da conta: " + conta);
+
+            /*aqui ele faz isso: double quantia = double.Parse(Console.ReadLine(),conta.Deposito(quantia);
+            nao sei se tem diferença real (no sacar tbm)*/
+            Console.Write("Entre um valor para depósito: ");
+            conta.depositar(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados da conta atualizado: " + conta);
+
+            Console.Write("Entre um valor para saque: ");
+            conta.sacar(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados da conta atualizado:  " + conta);
+        }
+    }
+}
